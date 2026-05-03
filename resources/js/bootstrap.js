beb9f6403e97230d80ee820fef19a538ef1fd479
Muhammad Yunus <muhammadyunus1072@gmail.com>
2026-05-03 17:02:59 +0700

    update

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -19,16 +19,29 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+
+//     wsHost: window.location.hostname,   // ✅ IMPORTANT FIX
+
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+
+//     forceTLS: window.location.protocol === 'https:',
+//     enabledTransports: ['ws', 'wss'],
+// });
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
 
-    wsHost: window.location.hostname,   // ✅ IMPORTANT FIX
+    wsHost: import.meta.env.VITE_REVERB_HOST,
 
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
 
-    forceTLS: window.location.protocol === 'https:',
+    forceTLS: true,
     enabledTransports: ['ws', 'wss'],
 });
 // import Echo from 'laravel-echo';
