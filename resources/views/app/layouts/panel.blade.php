bab2762cc8c4d5b2e6f8d4a4de2d99e8d4cc1eca
Muhammad Yunus <muhammadyunus1072@gmail.com>
2026-05-04 13:31:03 +0700

    udpate

--- resources/views/app/layouts/panel.blade.php
+++ resources/views/app/layouts/panel.blade.php
@@ -25,7 +25,7 @@
     <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
     <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" type="text/css" />
     {{-- <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" type="text/css" /> --}}
     <!--end::Global Stylesheets Bundle-->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
--- resources/views/app/layouts/public.blade.php
+++ resources/views/app/layouts/public.blade.php
@@ -24,7 +24,7 @@
         <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
         <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
         <link href="{{ asset('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet" type="text/css" />
       <style>
          .material-symbols-outlined {
          font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24;
