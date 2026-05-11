ecb57745076e4c8f72228d0c00d09cd1ea2454cd
Muhammad Yunus <muhammadyunus1072@gmail.com>
2026-05-11 10:59:56 +0700

    dat

--- resources/views/app/gensen-form/gensen-data/export.blade.php
+++ resources/views/app/gensen-form/gensen-data/export.blade.php
@@ -67,8 +67,8 @@
                     <td>{{ $data['has_kartu_keluarga'] ? 'O' : 'X' }}</td>
                     <td></td>
                     @php
-                        $total_amounts = explode(';', $item['remittance_total_amounts']);
-                        $receiver_names = explode(';', $item['remittance_receiver_names']);
+                        $total_amounts = explode(';', $data['remittance_total_amounts']);
+                        $receiver_names = explode(';', $data['remittance_receiver_names']);
                         logger(['export total amounts', $total_amounts])
                         logger(['export receivers', $receiver_names])
                     @endphp
