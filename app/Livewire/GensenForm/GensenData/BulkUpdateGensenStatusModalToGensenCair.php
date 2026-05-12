92b0d5f167a18cc8b5a0e6aa48d38e75e8464358
Muhammad Yunus <muhammadyunus1072@gmail.com>
2026-05-12 13:09:03 +0700

    udpate

--- app/Livewire/GensenForm/GensenData/BulkUpdateGensenStatusModalToGensenCair.php
+++ app/Livewire/GensenForm/GensenData/BulkUpdateGensenStatusModalToGensenCair.php
@@ -110,11 +110,12 @@ class BulkUpdateGensenStatusModalToGensenCair extends Component
                     $gensenForm = GensenFormRepository::findBy([
                         ['no_input_jepang', $value['data']['no_input_jepang']]
                     ]);
+                    preg_match('/^\d+/', trim($value['data']['tahun_gensen']), $tahun_reiwa);
                     $updated = GensenFormDetailRepository::updateBy([
                         // ['id_customer', $value['data']['id_customer']],
                         ['gensen_form_id', $gensenForm->id],
                         ['nama_lengkap', $value['data']['nama_lengkap']],
-                        ['tahun_gensen', $value['data']['tahun_gensen']],
+                        ['tahun_gensen', $tahun_reiwa[0]],
                         ['no_input_jepang', $value['data']['no_input_jepang']],
                         ['tanggal_pengajuan', $value['data']['tanggal_pengajuan']],
                     ], [
