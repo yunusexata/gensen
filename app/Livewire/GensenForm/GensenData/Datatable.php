afd96b31984ebe11d41c188cbcf6e8434c84631e
Muhammad Yunus <muhammadyunus1072@gmail.com>
2026-05-12 14:39:37 +0700

    update

--- app/Livewire/GensenForm/GensenData/Datatable.php
+++ app/Livewire/GensenForm/GensenData/Datatable.php
@@ -361,10 +361,18 @@ class Datatable extends Component
                     $total_amounts = explode(';', $item->remittance_total_amounts);
                     $receiver_names = explode(';', $item->remittance_receiver_names);
                     $receiver_years = explode(';', $item->remittance_receiver_years);
+                    if ($item->remittance_total_amounts) {
 
+                        dd(
+                            $total_amounts,
+                            $receiver_names,
+                            $receiver_years
+                        );
+                        dd($receiver_years);
+                    }
                     $html = "";
                     foreach ($total_amounts as $index => $amount) {
-                        $html .= "<div class='text-nowrap'>" . $amount . " - {$receiver_names[$index]} - {$receiver_years[$index]} " . toReiwaYear($receiver_years[$index]) . "</div>";
+                        $html .= "<div class='text-nowrap'>" . $amount . " - {$receiver_names[$index]} - " . toReiwaYear($receiver_years[$index]) . "</div>";
                     }
                     return $html;
                 }
