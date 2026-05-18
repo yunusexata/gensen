1cf60f16f9dc6c3195e1dc0b91b8b747b97fcbc7
yunusexata <yunusexata@gmail.com>
2026-05-18 15:25:31 +0700

    icon

--- resources/views/app/layouts/panel.blade.php
+++ resources/views/app/layouts/panel.blade.php
@@ -7,6 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', env('APP_NAME'))</title>
 
+    <!-- Standard favicon -->
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset(config('template.logo_panel')) }}">
+    <link rel="icon" href="{{ asset(config('template.logo_panel')) }}" type="image/x-icon">
+    
+    <!-- Apple devices -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset(config('template.logo_panel')) }}">
+
+    <!-- Shortcut icon -->
+    <link rel="shortcut icon" href="{{ asset(config('template.logo_panel')) }}">
+    
     <!--begin::Fonts(mandatory for all pages)-->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
     <!--end::Fonts-->
