2400cfe0e3bb766759dea15f354013e890c749b1
yunusexata <yunusexata@gmail.com>
2026-05-19 16:37:19 +0700

    autgg

--- app/Repositories/Gensen/GensenExportImportHistoryRepository.php
+++ app/Repositories/Gensen/GensenExportImportHistoryRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Gensen;
 
 use App\Models\Gensen\GensenExportImportHistory;
 use App\Repositories\MasterDataRepository;
+use Illuminate\Support\Facades\Auth;
 
 class GensenExportImportHistoryRepository extends MasterDataRepository
 {
@@ -14,6 +15,11 @@ class GensenExportImportHistoryRepository extends MasterDataRepository
 
     public static function datatable()
     {
-        return GensenExportImportHistory::query();
+
+        $pic_code = Auth::user()->pic_code;
+        return GensenExportImportHistory::query()
+            ->when($pic_code, function ($q) use ($pic_code) {
+                $q->where('created_by', Auth::user()->id);
+            });
     }
 }
