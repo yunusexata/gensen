291720ca75ea0b4c3e9c9bfc6e22299a1cb5c907
yunusexata <yunusexata@gmail.com>
2026-05-19 14:55:38 +0700

    blob

--- app/AiServices/Services/GeminiService.php
+++ app/AiServices/Services/GeminiService.php
@@ -60,6 +60,7 @@ class GeminiService
                 data: base64_encode($data)
             );
         })->toArray();
+        logger(['blobs', $blobs]);
         return [];
 
         // High-precision configuration for tax data
