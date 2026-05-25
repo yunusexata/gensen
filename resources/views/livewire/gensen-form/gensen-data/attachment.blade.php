d2c394a6b17afdee91dc00306db5300c9c3f48b3
yunusexata <yunusexata@gmail.com>
2026-05-25 09:34:30 +0700

    update

--- resources/views/livewire/gensen-form/gensen-data/attachment.blade.php
+++ resources/views/livewire/gensen-form/gensen-data/attachment.blade.php
@@ -883,8 +883,7 @@
                                                 <!-- Preview -->
                                                 <a wire:ignore
                                                     data-fslightbox="{{ $item['id'] }}"
-                                                    data-type="iframe"
-                                                    href="#{{ $item['id'] }}"
+                                                    href="{{ $item['url'] }}"
                                                     class="block thumbnail-aspect bg-surface-container rounded-lg overflow-hidden"
                                                 >
                                                     <img
@@ -896,13 +895,13 @@
                                                 <!-- Actions -->
                                                 <div class="absolute top-1 right-1 z-10">
                                                     <!-- Tag A (Download) -->
-                                                    {{-- <a 
+                                                    <a 
                                                         href="{{ $item['url'] }}"
                                                         download=""
                                                         class="inline-flex items-center justify-center p-1 bg-white/80 hover:bg-primary/10 text-primary rounded h-8 w-8 transition-colors"
                                                     >
                                                         <span class="material-symbols-outlined text-xl">save</span>
-                                                    </a> --}}
+                                                    </a>
 
                                                     <!-- Tag Button (Delete) -->
                                                     <button 
@@ -912,16 +911,6 @@
                                                     >
                                                         <span class="material-symbols-outlined text-xl">delete</span>
                                                     </button>
-                                                </div>
-                                                
-                                                <div style="position:absolute; left:-9999px; top:-9999px;">
-                                                    <iframe
-                                                        src="{{$item['url']}}"
-                                                        id="{{$item['id']}}"
-                                                        width="1920"
-                                                        height="1080"
-                                                        frameborder="0"
-                                                    ></iframe>
                                                 </div>
 
                                             </div>
