f0e4c9bb4ef0c51c7fa76bb6bfdfb837b0d372ab
yunusexata <yunusexata@gmail.com>
2026-05-28 14:45:28 +0700

    asd

--- app/Livewire/GensenForm/GensenData/Datatable.php
+++ app/Livewire/GensenForm/GensenData/Datatable.php
@@ -401,14 +401,17 @@ class Datatable extends Component
 
                     $tarik_data_details = explode(';', $item->tarik_data_details);
 
-                    $detail = $item->gensenFormDetails()
+                    $q = $item->gensenFormDetails()
                         ->whereNull('deleted_at')
                         ->whereNull('tanggal_tarik_data')
                         ->where(function ($q) {
                             $q->whereNull('label')
                                 ->orWhere('label', '=', '');
-                        })->get()->toArray();
-                    $html = json_encode($detail, true);
+                        });
+                    $html = json_encode([
+                        $q->exists(),
+                        $q->count()
+                    ], true);
                     foreach ($tarik_data_details as $index => $data) {
                         $html .= "<div class='text-nowrap'>" . ($item->allGensenDetailsTarikData() ? 'TARIK' : 'KOSONG') . " {$data}</div>";
                     }
