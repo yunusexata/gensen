01e55a0cbe06ccf063817b1fc1cbf7ca7ead9a35
yunusexata <yunusexata@gmail.com>
2026-05-29 15:45:50 +0700

    asd

--- app/Http/Controllers/IchijikinExtraction/IchijikinExtractionController.php
+++ app/Http/Controllers/IchijikinExtraction/IchijikinExtractionController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\IchijikinExtraction;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class IchijikinExtractionController extends Controller
 {
     public function index()
     {
         return view('app.ichijikin-extraction.index');
+    }
+    public function create()
+    {
+        return view('app.ichijikin-extraction.detail', ["objId" => null]);
+    }
+
+    public function edit(Request $request)
+    {
+        return view('app.ichijikin-extraction.detail', ["objId" => $request->id]);
     }
 }
