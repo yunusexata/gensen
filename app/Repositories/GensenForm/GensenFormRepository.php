a6492fbef1450d9f6b6fc6ca3123b2544c7a5687
yunusexata <yunusexata@gmail.com>
2026-05-29 10:46:33 +0700

    asd

--- app/Repositories/GensenForm/GensenFormRepository.php
+++ app/Repositories/GensenForm/GensenFormRepository.php
@@ -273,7 +273,7 @@ class GensenFormRepository extends MasterDataRepository
             ->leftJoinSub($gensenDetailAgg, 'gfd', function ($join) {
                 $join->on('gfd.gensen_form_id', '=', 'gensen_forms.id');
             })
-            ->leftJoinSub($gensenDetailCairAgg, 'gfd_filter_cair', function ($join) {
+            ->leftJoinSub($gensenDetailCairAgg, 'gfd_cair', function ($join) {
                 $join->on('gfd_cair.gensen_form_id', '=', 'gensen_forms.id');
             })
             ->select([
