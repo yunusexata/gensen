59b075441c7f1b2ecfa0c45f0001631afc7f5c2b
yunusexata <yunusexata@gmail.com>
2026-06-03 15:25:38 +0700

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,6 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        // Schedule::command('telescope:prune --hours=48')
+        //     ->dailyAt('02:00');
         // $schedule->command('inspire')->hourly();
     }
 
@@ -20,7 +22,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
