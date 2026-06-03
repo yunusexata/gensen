53cf49bb12b89159a0cc3b92ca92408e86928ea2
yunusexata <yunusexata@gmail.com>
2026-06-03 14:09:26 +0700

    asd

--- app/Livewire/IchijikinExtraction/DatatableBatchDetail.php
+++ app/Livewire/IchijikinExtraction/DatatableBatchDetail.php
@@ -117,7 +117,7 @@ class DatatableBatchDetail extends Component
                 'render' => function ($item) {
 
                     $url = asset('storage/ichijikin/' . $item->ichijikinExtraction->batch_name . '/crop/' . $item->file_stored_name . '/nama_lengkap.png');
-                    return "$item->nama_lengkap<br><img src='" . $url . "' style='width:450px  !important; border:1px solid black; border-radius:5px;'>";
+                    return "$item->nama_lengkap<br><img src='" . $url . "' style='width:100%; border:1px solid black; border-radius:5px;'>";
                 }
             ],
             [
