3ab8802b6564e4f4fc3effe661895653db2bde2e
yunusexata <yunusexata@gmail.com>
2026-06-04 08:59:49 +0700

    sad

--- resources/views/livewire/ichijikin-extraction/edit.blade.php
+++ resources/views/livewire/ichijikin-extraction/edit.blade.php
@@ -2,7 +2,7 @@
 
     <form wire:submit.prevent="store">
         
-    <div wire:loading wire:target="images, store, file_ichijikin"
+    <div wire:loading wire:target="images, store, file_ichijikin, storeExtract"
      class="position-fixed top-0 start-0 w-100 h-100 
             bg-dark bg-opacity-50 
             justify-content-center align-items-center"
