bb487f0c48890a4775a1563331ab75d27ab46555
yunusexata <yunusexata@gmail.com>
2026-06-24 09:08:38 +0700

    asd

--- resources/views/app/gensen/gensen-data/export.blade.php
+++ resources/views/app/gensen/gensen-data/export.blade.php
@@ -77,8 +77,8 @@
                         <td>{{ $data['tanggal_tarik_data_detail'] ? Carbon\Carbon::parse($data['tanggal_tarik_data_detail'])->format('Ymd') : '' }}</td>
                         <td>{{ $data['label_detail'] }}</td>
                         <td>{{ $data['tanggal_cair'] ? Carbon\Carbon::parse($data['tanggal_cair'])->format('Ymd') : '' }}</td>
-                        <td style='mso-number-format:"\#\.\#\#0";'>
-                            {{ $data['nominal_cair'] }}
+                        <td style='mso-number-format:"\@";'>
+                            {{ number_format((float) $data['nominal_cair'], 0, ',', '.') }}
                         </td>
                         <td>{{ $data['status']}}</td>
                         <td>{{ $data['keterangan']}}</td>
@@ -90,8 +90,8 @@
                         <td>{{ $data['tahun_gensen_detail'] }}</td>
                         <td>{{ $data['has_my_number'] ? 'O' : 'X' }}</td>
                         <td>{{ $data['has_kartu_keluarga'] ? 'O' : 'X' }}</td>
-                        <td style='mso-number-format:"\#\.\#\#0";'>
-                            {{ $data['nominal_gensen_detail'] }}
+                        <td style='mso-number-format:"\@";'>
+                            {{ number_format((float) $data['nominal_gensen_detail'], 0, ',', '.') }}
                         </td>
                         @php
                             $total_amounts = explode(';', $data['remittance_total_amounts']);
